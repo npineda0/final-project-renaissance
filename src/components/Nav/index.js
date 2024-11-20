c5fba2af5b368edd0077ab9d1649676f34c4ab43
fralcala <fralcala@iu.edu>
2024-11-20 10:56:28 -0500

    created nav component and added color palette

---
+++ src/components/Nav/index.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import navStyles from "./nav.module.css";
+
+export default function Nav() {
+  return (
+    <nav>
+      <ul className={navStyles.mainNav}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/">link</Link>
+        </li>
+        <li>
+          <Link href="/">link</Link>
+        </li>
+        
+        
+      </ul>
+    </nav>
+  );
+}
