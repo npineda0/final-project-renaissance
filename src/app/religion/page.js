d3b006eb82876527095deb08b1f3947231a6c0aa
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2024-12-07 22:34:21 -0500

    Clerk
    Implemented clerk to hide content from signed out user's.

--- src/app/religion/page.js
+++ src/app/religion/page.js
@@ -1,23 +1,26 @@
 import relStyles from "./religion.module.css";
 import Learn from "@/components/LearnButton";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default function Religion() {
   return (
     <main className={relStyles.religion}>
       <h1>Religion</h1>
       <div className={relStyles.relContent}>
-        <div className={relStyles.r1}>
-          <h2>Theology</h2>
-          <p>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Conubia
-            suscipit mollis suscipit torquent aliquet ridiculus? Varius ad
-            suscipit est ultricies penatibus. Natoque purus nullam velit ligula
-            penatibus ultricies nec. Nulla nisl tortor nunc ullamcorper aliquet
-            amet. Conubia efficitur lobortis montes ullamcorper pulvinar netus
-            accumsan.
-          </p>
-          <Learn link={"/religion/theology"}></Learn>
-        </div>
+        <SignedIn>
+          <div className={relStyles.r1}>
+            <h2>Theology</h2>
+            <p>
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Conubia
+              suscipit mollis suscipit torquent aliquet ridiculus? Varius ad
+              suscipit est ultricies penatibus. Natoque purus nullam velit
+              ligula penatibus ultricies nec. Nulla nisl tortor nunc ullamcorper
+              aliquet amet. Conubia efficitur lobortis montes ullamcorper
+              pulvinar netus accumsan.
+            </p>
+            <Learn link={"/religion/theology"}></Learn>
+          </div>
+        </SignedIn>
         <div className={relStyles.r2}>
           <h2>Important Figures</h2>
           <p>
@@ -30,18 +33,20 @@ export default function Religion() {
           </p>
           <Learn link={"/religion/people"}></Learn>
         </div>
-        <div className={relStyles.r3}>
-          <h2>Philosophy</h2>
-          <p>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Conubia
-            suscipit mollis suscipit torquent aliquet ridiculus? Varius ad
-            suscipit est ultricies penatibus. Natoque purus nullam velit ligula
-            penatibus ultricies nec. Nulla nisl tortor nunc ullamcorper aliquet
-            amet. Conubia efficitur lobortis montes ullamcorper pulvinar netus
-            accumsan.
-          </p>
-          <Learn link={"/religion/philosophy"}></Learn>
-        </div>
+        <SignedIn>
+          <div className={relStyles.r3}>
+            <h2>Philosophy</h2>
+            <p>
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Conubia
+              suscipit mollis suscipit torquent aliquet ridiculus? Varius ad
+              suscipit est ultricies penatibus. Natoque purus nullam velit
+              ligula penatibus ultricies nec. Nulla nisl tortor nunc ullamcorper
+              aliquet amet. Conubia efficitur lobortis montes ullamcorper
+              pulvinar netus accumsan.
+            </p>
+            <Learn link={"/religion/philosophy"}></Learn>
+          </div>
+        </SignedIn>
       </div>
     </main>
   );
