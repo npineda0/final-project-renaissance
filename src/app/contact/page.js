e92dfc186066ff77874186ae44005c75e7070de7
Denisse <denissehenriquez@hotmail.com>
2024-12-09 14:21:58 -0500

    put the information for the contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,6 +4,22 @@ export default function contact() {
   return (
     <main>
       <h1>Contact</h1>
+
+      <form>
+        <label>Name</label>
+        <input></input>
+
+        <label>Please select a topic</label>
+        <select>
+          <option>Literature</option>
+          <option>Religion</option>
+          <option>Fine Arts</option>
+          <option>Music</option>
+        </select>
+
+        <label>Please type your questions or reccomandation you have</label>
+        <textarea></textarea>
+      </form>
     </main>
   );
 }
