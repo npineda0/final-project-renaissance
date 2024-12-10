93b4f7cd736b0015e72336f55e903a4dfa22bfb7
Denisse <denissehenriquez@hotmail.com>
2024-12-10 12:43:18 -0500

    add information to the music and added a music componet

--- src/app/music/page.js
+++ src/app/music/page.js
@@ -4,6 +4,11 @@ export default function music() {
   return (
     <main>
       <h1>Music</h1>
+      <p>
+        Renaissance Music is European music from the 15th and 16th centuries
+        that is known for its use of poylphony, intricate compositions, and a
+        shift from sacred to secular music
+      </p>
     </main>
   );
 }
