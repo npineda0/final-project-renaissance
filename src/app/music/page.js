b74684c0f5196337eca3161dbc822af779bfe0e0
Denisse <denissehenriquez@hotmail.com>
2024-12-10 12:50:43 -0500

    add information to the music page and also created folder for routing

--- src/app/music/page.js
+++ src/app/music/page.js
@@ -9,6 +9,18 @@ export default function music() {
         that is known for its use of poylphony, intricate compositions, and a
         shift from sacred to secular music
       </p>
+
+      <div>
+        <h3>Greensleeves</h3>
+      </div>
+
+      <div>
+        <h3>Missa Papae Marcelli</h3>
+      </div>
+
+      <div>
+        <h3>Madrigal</h3>
+      </div>
     </main>
   );
 }
