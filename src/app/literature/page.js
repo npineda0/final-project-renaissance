28d200d1fd575cf56e8f14d8b693d90c57129305
npineda0 <97705502+npineda0@users.noreply.github.com>
2024-12-09 22:19:28 -0500

    Images added to public folder & Lit content edited

--- src/app/literature/page.js
+++ src/app/literature/page.js
@@ -3,7 +3,13 @@ import literatureStyles from "./literature.module.css";
 export default function literature() {
   return (
     <main>
-      <h1>Literature</h1>
+      <h1 className={literatureStyles.header}>Literature</h1>
+      <p className={literatureStyles.litDesc}>
+        Renaissance literature is characterized by the adoption of a Humanist
+        philosophy and the recovery of the classical literature of Antiquity,
+        and benefited from the spread of printing in the latter part of the 15th
+        century. Here are some of the most notable works during this time:
+      </p>
     </main>
   );
 }
