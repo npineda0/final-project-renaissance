1c8899968f7926e6efe3f3e619b7191903138468
Denisse <denissehenriquez@hotmail.com>
2024-12-10 19:02:35 -0500

    finish with the contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,28 +1,35 @@
 "use client";
 import contactStyles from "./contact.module.css";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default function contact() {
   return (
     <main className={contactStyles.renaissance}>
       <h1>Contact</h1>
 
-      <form>
-        <label>Name</label>
-        <input></input>
+      <SignedIn>
+        <form>
+          <label>Name</label>
+          <input></input>
 
-        <label>Please select a topic</label>
-        <select>
-          <option>Literature</option>
-          <option>Religion</option>
-          <option>Fine Arts</option>
-          <option>Music</option>
-        </select>
+          <label>Please select a topic</label>
+          <select>
+            <option>Literature</option>
+            <option>Religion</option>
+            <option>Fine Arts</option>
+            <option>Music</option>
+          </select>
 
-        <label>Please type your questions or reccomandation you have</label>
-        <textarea></textarea>
+          <label>Please type your questions or reccomandation you have</label>
+          <textarea></textarea>
 
-        <button>Submit</button>
-      </form>
+          <button>Submit</button>
+        </form>
+      </SignedIn>
+
+      <SignedOut>
+        <h1>Please sign in</h1>
+      </SignedOut>
     </main>
   );
 }
