42d8bba21541c38a755a54dd4e508c411261c795
Denisse <denissehenriquez@hotmail.com>
2024-12-13 19:35:03 -0500

    added the style to all of the music page

--- src/components/musicLayout/index.js
+++ src/components/musicLayout/index.js
@@ -1,12 +1,13 @@
 "use client";
 import Link from "next/link";
+import musicStyle from "../musicLayout/musiclayout.module.css";
 
 export default function Music({ title = "", images = "", text = "" }) {
   return (
-    <div>
+    <div className={musicStyle.layoutDesign}>
       <h3>{title}</h3>
       <img src={images} alt={title} />
-      <h3>{text}</h3>
+      <h5>{text}</h5>
     </div>
   );
 }
