7be5366b0a5458c42cb348eca3c77040445c17d4
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2024-12-13 22:27:49 -0500

    Art  and Architecture fix

--- src/app/religion/art/page.js
+++ src/app/religion/art/page.js
@@ -6,51 +6,33 @@ export default function Art() {
   return (
     <main className={artStyles.main}>
       <h1>Religious Art and Architecture</h1>
-      {/* <p>
-        During the 13th and 14th century Religious Art was used in a number of
-        ways. Churches were huge patrons of the arts and purchased tremendous
-        amounts of art to create awe and respect. Making this art available to
-        the public helped many people who could not read the scriptures by
-        allowing them to envision the stories and relate to the subjects.
-        Churches also hoped to inspire greater devotion to religion and arouse
-        more desire for salvation. Early Renaissance artists began using more
-        humanistic looking subjects and naturalistic settings to achieve those
-        goals. By placing viewers in the scenes with the religious icons, the
-        allegories became more relevant to everyday life. Throughout the
-        Renaissance artists continued to develop new styles and techniques that
-        appealed to and inspired the masses.
-      </p> */}
       <section className={artStyles.gallery}>
-        <BasicInfo
-          img="/creation-of-adam.jpg"
-          header="Creation of Adam"
-          subheader="1512"
-          nested={"none"}
-        ></BasicInfo>
-        <BasicInfo
-          img="/judas-kiss.jpg"
-          header="The Kiss of Judas"
-          subheader="1306, Giotto di Bondone"
-          nested={"none"}
-        ></BasicInfo>
-        <BasicInfo
-          img="/last-supper.jpg"
-          header="Last Supper"
-          subheader="1498, Leonardo Da Vinci"
-          nested={"none"}
-        ></BasicInfo>
-        <BasicInfo
-          img="/madonna.jpg"
-          header="Sistine Madonna"
-          subheader="1512, Raphael"
-          nested={"none"}
-        ></BasicInfo>
-        <BasicInfo
-          img="/venus-birth.jpg"
-          header="Creation of Adam"
-          subheader="1485, Sandro Botticelli"
-          nested={"none"}
-        ></BasicInfo>
+        <section className={artStyles.sh1}>
+          <img src="/creation-of-adam.jpg" height={200} />
+          <p className={artStyles.header}>Creation of Adam</p>
+          <p>1512, Michelangelo Buonarroti</p>
+        </section>
+        <section className={artStyles.sh1}>
+          <img src="/judas-kiss.jpg" height={200} />
+          <p className={artStyles.header}>The Kiss of Judas</p>
+          <p>1306, Giotto di Bondone</p>
+        </section>
+        <section className={artStyles.sh1}>
+          <img src="/last-supper.jpg" height={200} />
+          <p className={artStyles.header}>Last Supper</p>
+          <p>1498, Leonardo Da Vinci</p>
+        </section>
+
+        <section className={artStyles.sh1}>
+          <img src="/madonna.jpg" height={200} />
+          <p className={artStyles.header}>Sistine Madonna</p>
+          <p>1512, Raphael</p>
+        </section>
+        <section className={artStyles.sh1}>
+          <img src="/venus-birth.jpg" height={200} />
+          <p className={artStyles.header}>The Birth of Venus</p>
+          <p>1485, Sandro Botticelli</p>
+        </section>
       </section>
     </main>
   );
